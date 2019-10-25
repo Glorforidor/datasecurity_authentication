3709e05f352b65e1cf3e67fc92d22154233b2cd2
Peter Bom Jakobsen <pejak1990@gmail.com>
2019-10-26 00:10:53 +0200

    implemented a small CLI
    Signed-off-by: Peter Bom Jakobsen <pejak1990@gmail.com>

--- src/datasecurity_authentication/Client.java
+++ src/datasecurity_authentication/Client.java
@@ -1,43 +1,97 @@
 package datasecurity_authentication;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.util.Map;
 
-public class Main {
+public class Client {
 	public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
 		PrintService service = (PrintService) Naming.lookup("rmi://localhost:5099/print");
-		service.start();
-
-		service.print("MySecretDocument", "DTUPrinter");
-		service.print("MySecretDocument1", "DTUPrinter1");
-		service.print("MySecretDocument2", "DTUPrinter2");
-		service.print("MySecretDocument3", "DTUPrinter3");
-
-		Map<Integer, String> queue = service.queue();
-		for (Map.Entry<Integer, String> m: queue.entrySet()) {
-			System.out.println(String.format("Job number: %d, filename: %s", m.getKey(), m.getValue()));
+		try {
+			new CLI(service).run();;
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
-		service.topQueue(4);
+		System.out.println("Bye");
+	}
 
-		queue = service.queue();
-		for (Map.Entry<Integer, String> m: queue.entrySet()) {
-			System.out.println(String.format("Job number: %d, filename: %s", m.getKey(), m.getValue()));
+	static class CLI {
+		PrintService srv;
+
+		public CLI(PrintService service) {
+			this.srv = service;
 		}
 
-		service.setConfig("print_toner", "black");
-		System.out.println(String.format("The print toner is: %s", service.readConfig("print_toner")));
+		void menu() {
+			System.out.println("Crypto Printer v0.0.1");
+			System.out.println("=".repeat(100));
+			System.out.println("Menu");
+			System.out.println("-".repeat(100));
+			System.out.println("(1) Status");
+			System.out.println("(2) Print");
+			System.out.println("(3) Queue");
+			System.out.println("(4) topQueue");
+			System.out.println("(5) Start");
+			System.out.println("(6) Stop");
+			System.out.println("(7) Restart");
+			System.out.println("(8) Quit");
+		}
 
-		System.out.println(service.status());
-		service.start();
-		service.restart();
-
-		queue = service.queue();
-		System.out.println(service.status());
-
-		service.stop();
+		void run() throws IOException {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			menu();
+			loop: while (true) {
+				var input = br.readLine();
+				switch (input) {
+				case "1":
+					System.out.println(srv.status());
+					break;
+				case "2":
+					System.out.print("Write input file: ");
+					var filename = br.readLine();
+					System.out.print("Write printer: ");
+					var printer = br.readLine();
+					try {
+						srv.print(filename, printer);
+					} catch (RemoteException e) {
+						System.out.println("Seems the printer is not running, try start it first");
+					}
+					break;
+				case "3":
+					var queue = srv.queue();
+					for (Map.Entry<Integer, String> q : queue.entrySet()) {
+						System.out.println(String.format("Job number: %d, filename: %s", q.getKey(), q.getValue()));
+					}
+					break;
+				case "4":
+					System.out.print("Job number: ");
+					var job = br.readLine();
+					srv.topQueue(Integer.parseInt(job));
+					break;
+				case "5":
+					srv.start();
+					break;
+				case "6":
+					srv.stop();
+					break;
+				case "7":
+					srv.restart();
+					break;
+				case "8":
+					break loop;
+				case "?":
+					menu();
+					break;
+				default:
+					System.out.println("Wrong input");
+				}
+			}
+		}
 	}
 }
