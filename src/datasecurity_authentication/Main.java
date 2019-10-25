9b4bdcdcbe78d233692bb81578f6f78b6e3f41a6
Peter Bom Jakobsen <pejak1990@gmail.com>
2019-10-25 14:11:05 +0200

    testing of the print server functionallity
    Signed-off-by: Peter Bom Jakobsen <pejak1990@gmail.com>

--- src/datasecurity_authentication/Main.java
+++ src/datasecurity_authentication/Main.java
@@ -1,7 +1,30 @@
 package datasecurity_authentication;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.Map;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+	public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
+		PrintService service = (PrintService) Naming.lookup("rmi://localhost:5099/print");
+
+		service.print("MySecretDocument", "DTUPrinter");
+		service.print("MySecretDocument1", "DTUPrinter1");
+		service.print("MySecretDocument2", "DTUPrinter2");
+		service.print("MySecretDocument3", "DTUPrinter3");
+
+		Map<Integer, String> queue = service.queue();
+		for (Map.Entry<Integer, String> m: queue.entrySet()) {
+			System.out.println(String.format("Job number: %d, filename: %s", m.getKey(), m.getValue()));
+		}
+
+		service.topQueue(4);
+
+		queue = service.queue();
+		for (Map.Entry<Integer, String> m: queue.entrySet()) {
+			System.out.println(String.format("Job number: %d, filename: %s", m.getKey(), m.getValue()));
+		}
 	}
 }
