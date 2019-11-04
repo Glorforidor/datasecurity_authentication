c5a8a73060d3f8d8d07c46de34585aaf88c3cd2d
Peter Bom Jakobsen <pejak1990@gmail.com>
2019-11-04 13:37:18 +0100

    make EnryptionHandler singleton
    Signed-off-by: Peter Bom Jakobsen <pejak1990@gmail.com>

--- src/datasecurity_authentication/EncryptionHandler.java
+++ src/datasecurity_authentication/EncryptionHandler.java
@@ -1,12 +1,34 @@
 package datasecurity_authentication;
 
 import java.security.Key;
+import java.security.SecureRandom;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class EncryptionHandler {
     private static String algo = "AES/CBC/PKCS5Padding";
+    private static EncryptionHandler instance;
+    public static SecretKeySpec key;
+
+    private EncryptionHandler() {
+        SecureRandom rand = new SecureRandom();
+        byte[] keyBytes = new byte[16];
+        rand.nextBytes(keyBytes);
+
+        // create symmetric key
+        key = new SecretKeySpec(keyBytes, 0, keyBytes.length, "AES");
+    }
+
+    public static EncryptionHandler getInstance() {
+        if (instance == null) {
+            instance = new EncryptionHandler(); 
+        }
+
+        return instance;
+    }
+
 
     /**
      * encrypt encrypts string messages and return a new Message with encrypted data and the IV.
@@ -15,21 +37,21 @@ public class EncryptionHandler {
      * @return a new Message
      * @throws Exception
      */
-    public static Message encrypt(Key secretKey, String plaintext) throws Exception {
-        return encrypt(secretKey, plaintext.getBytes("UTF-8"));
+    public Message encrypt(String plaintext) throws Exception {
+        return encrypt(plaintext.getBytes("UTF-8"));
     }
 
-    public static Message encrypt(Key secretKey, byte[] plaintext) throws Exception{
+    public Message encrypt(byte[] plaintext) throws Exception{
         Cipher cipher = Cipher.getInstance(algo);
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
 
         byte[] data = cipher.doFinal(plaintext);
         return new Message(data, cipher.getIV());
     }
 
-    public static String decrypt(Key secretKey, Message msg) throws Exception {
+    public String decrypt(Message msg) throws Exception {
         Cipher cipher = Cipher.getInstance(algo);
-        cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(msg.getIv()));
+        cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(msg.getIv()));
 
         byte[] data = cipher.doFinal(msg.getData());
         return new String(data);
