7cb199ba4c9ffb953aa8bd2f26b81c7181983bdd
Peter Bom Jakobsen <pejak1990@gmail.com>
2019-11-05 22:05:09 +0100

    add more documentation for PrintService
    Signed-off-by: Peter Bom Jakobsen <pejak1990@gmail.com>

--- src/datasecurity_authentication/PrintService.java
+++ src/datasecurity_authentication/PrintService.java
@@ -4,12 +4,15 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.Map;
 
+/**
+ * PrintService define the method signatures for a Print Server.
+ */
 public interface PrintService extends Remote {
     /**
      * print prints file filename on the specific printer.
      * @param filename file to print.
      * @param printer printer to use.
-     * @throws 
+     * @throws RemoteException
      */
     void print(String filename, String printer, Message msg) throws RemoteException;
 
@@ -17,6 +20,7 @@ public interface PrintService extends Remote {
      * queue lists the printer queue by jobnumber and filename.
      * @param printer the printer to get queue from.
      * @return Map<Integer, String>
+     * @throws RemoteException
      */
     Map<Integer, String> queue(String printer, Message msg) throws RemoteException;
 
@@ -70,7 +74,19 @@ public interface PrintService extends Remote {
      */
     void setConfig(String parameter, String value, Message msg) throws RemoteException;
 
-    Message login(Message msg) throws RemoteException;
+    /**
+     * login log in a user.
+     * @param message the Message containing information about a user.
+     * @return a Message containing the new Session for the user.
+     * @throws RemoteException
+     */
+    Message login(Message message) throws RemoteException;
 
-    boolean logout(Message msg) throws RemoteException;
+    /**
+     * logout logs out a user.
+     * @param message the Message containing a user's session.
+     * @return true if the user is successfully logged out, otherwise false.
+     * @throws RemoteException
+     */
+    boolean logout(Message message) throws RemoteException;
 }
